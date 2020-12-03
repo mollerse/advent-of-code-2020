e1353c5e0ce16349187e1216a23d80beba6d3bd1
Stian Veum Møllersen <stian.moellersen@gmail.com>
2020-12-03 17:23:38 +0100

    Add a strjoin to string extensions

--- 2/string-extensions.js
+++ 2/string-extensions.js
@@ -9,6 +9,11 @@ function strsplit(splitter, str) {
   return wrapAggregate(unwrap(str).split(unwrap(splitter)).map(wrap));
 }
 
+function strjoin(joiner, aggr) {
+  return wrap(unwrap(aggr).body.map(unwrap).reverse().join(unwrap(joiner)));
+}
+
 module.exports = {
-  strsplit: aitFFI__F(2, 'strsplit', strsplit)
+  strsplit: aitFFI__F(2, 'strsplit', strsplit),
+  strjoin: aitFFI__F(2, 'strjoin', strjoin)
 }
